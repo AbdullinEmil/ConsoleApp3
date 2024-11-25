20f31c9c0ce0def56d7aae16e185d17af7aeddc4
Tike-Myson <Tike-Myson@DESKTOP-S8ETN94>
2024-11-25 15:16:32 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+[AttributeUsage(AttributeTargets.Method)]
+public class LogExecutionTimeAttribute : Attribute
+{
+    public void OnMethodExecuted(Action methodToExecute)
+    {
+        DateTime startTime = DateTime.Now;
+        methodToExecute();
+        DateTime endTime = DateTime.Now;
+
+        TimeSpan elapsedTime = endTime - startTime;
+        Console.WriteLine($"Метод {methodToExecute.Method.Name} выполнен за {elapsedTime.TotalMilliseconds} мс");
+    }
+}
+
+public class ExampleClass
+{
+    [LogExecutionTime]
+    public void SlowMethod()
+    {
+        Thread.Sleep(2000);
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        ExampleClass example = new ExampleClass();
+        example.SlowMethod();
+    }
+}
